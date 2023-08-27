4fd2009690afb96972f52051eae326a517d6e11a
桂震东 <138844770+Study-gui@users.noreply.github.com>
2023-08-27 19:32:50 +0800

    Add files via upload

--- Projtest_8_26/FileName.cpp
+++ Projtest_8_26/FileName.cpp
@@ -3,7 +3,7 @@
 
 
 
-//选择排序
+//插入排序
 void InsertSort(int* a, int n)
 {
 	for (int i = 0; i < n-1; i++)
@@ -88,13 +88,14 @@ void AdjustDwon(int* a, int n, int root)
 }
 //建堆
 void HeapSort(int* a, int n)
-{
+{//建堆，时间复杂度为O（N）
 	//从最下面往上面进行堆排,但是防止越界就从，倒数第二层开始
 	for(int i = (n - 1 - 1) / 2; i>=0; i--)
 	{
 		AdjustDwon(a, n, i);
 	}
 	//堆排序
+	//升序，采用建大堆，时间复杂度为O（N*logN）
 	int end = n - 1;
 	while (end >0)
 	{
@@ -103,6 +104,62 @@ void HeapSort(int* a, int n)
 		end--;
 	}
 }
+//快速排序,挖坑法
+void QuickSort(int* a, int left, int right)
+{
+	//递归结束条件
+	if (left >= right)
+	{
+		return;
+	}
+	int begin = left, end = right;
+	int pivot = begin;
+	int key = a[begin];
+	//一次循环的条件
+	while (begin<end)
+	{
+		//从右往左找小
+		while (begin<end && a[end] > key)
+		{
+			end--;
+		}
+		//把左边的坑填上，右边产生新的坑
+		a[pivot] = a[end];
+		pivot = end;
+		//从左往右找大
+		while (begin<end && a[begin] < key)
+		{
+			begin++;
+		}
+		//把右边的坑填上，左边产生新的坑
+		a[pivot] = a[begin];
+		pivot = begin;
+	}
+	//把最后的坑，用记录的key，填上
+	pivot = begin;
+	a[pivot] = key;
+	//采用分治,将两边再次递归、
+	//左边
+	QuickSort(a, left, pivot - 1);
+	//右边
+	QuickSort(a, pivot + 1, right);
+}
+//选择插入，时间复杂度为O（N^2）
+void SelectSort(int* a, int n)
+{
+	int left = 0, right = n-1;
+	while (left < right)//对数组进行遍历
+	{
+		if (a[left] > a[right])//如果左边小于右边，就进行交换
+		{
+			Swap(&a[left], &a[right]);
+		}
+		left++;
+		right--;
+	}
+
+}
+
 int main()
 {
 	int arr[] = { 9,8,7,6,5,4,3,2,1 };
@@ -113,7 +170,9 @@ int main()
 	//{
 	//	printf("%d ", arr[i]);
 	//}
-	HeapSort(arr, n);
+	//HeapSort(arr, n);
+	//QuickSort(arr, 0, n - 1);
+	SelectSort(arr, n);
 	for (int i = 0; i < n; i++)
 	{
 		printf("%d ", arr[i]);
